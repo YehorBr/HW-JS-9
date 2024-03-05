117b15627cb07518a2b5a862ea219a2f5758f772
Yehor <yehor12344321@gmail.com>
2024-03-05 20:25:19 +0200

    зберігає зміни

---
+++ index.js
@@ -0,0 +1,48 @@
+// const arrOfNums = [1, 2, 3];
+// arrOfNums[1] = 10
+// console.log(arrOfNums);
+
+// const arrOfStrings = ['a', 'b', 'c']; 
+// arrOfStrings[3] = 'd'
+// console.log(arrOfStrings);
+
+// const arrOfNums = [1, 2, 3, 4, 5];
+// let total = 0;
+// for (const num of arrOfNums) {
+//     total += num;
+
+// }
+// console.log(total);
+
+// const arrOfNums = [1, 2, 3, 4, 5];
+// for (const num of arrOfNums) {
+//     console.log(num);
+// }
+
+// const arrOfStrings = ['alphabet', 'abcd', 'Yehor', 'JavaScript', 'cake' ];
+
+// for (const string of arrOfStrings) {
+//     if(string.length > 5){
+//         console.log(string);
+//     }
+// }
+
+// const arrOfNums = [10, 12, 40, 124, 1, 4, 41, 23, 102, 1782 ];
+// let maxNum = arrOfNums[0]
+// for (const num of arrOfNums) {
+//     if(num > maxNum){
+//         maxNum = num;
+//     }
+// }
+
+// console.log(maxNum);
+
+// console.log(Math.max(...arrOfNums));
+
+// const arrOfNums = [12, 2, 3, 7, 9, 10, 4, 6, 11, 20];
+
+// for (const num of arrOfNums) {
+//     if(num % 2 === 0){
+//         console.log(num);
+//     }
+// }
